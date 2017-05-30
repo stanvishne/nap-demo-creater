48015fb3963c91412b52e43fde2d02c3ab508a0c
stan vishne <stanvishne@gmail.com>
2017-05-30 11:26:09 +0300

    removed unused files

--- src/components/todo/todo-list/todo-list.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TestUtils from 'react/lib/ReactTestUtils';
-import expect from 'expect';
-import TodoList from "./todo-list.jsx";
-
-describe('todo-list', () => {
-  it('TODO: I will write some tests');
-});
